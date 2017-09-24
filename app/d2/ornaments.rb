f6ff82d964fbe9d8f8adc2fba1a895190ffd6599
Phillip Jackson <unisys12@gmail.com>
2017-09-24 09:26:41 -0500

    add ornaments

---
+++ app/d2/ornaments.rb
@@ -0,0 +1,25 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'mongo'
+require 'csv'
+require 'dotenv/load'
+
+Mongo::Logger.level = Logger::FATAL
+
+client = Mongo::Client.new([ENV['DB_URL']], database: ENV['DB_USER'], user: ENV['DB_USER'], password: ENV['DB_PASSWORD'])
+
+item_defs = client['destiny2.manifest.en.DestinyInventoryItemDefinition']
+
+@ornaments = item_defs.find(itemCategoryHashes: 56)
+
+CSV.open('d2_ornaments_simple.csv', 'wb') do |csv|
+  csv << %w[name flavor_text image_url]
+  @ornaments.each do |ornament|
+    next unless ornament['itemTypeDisplayName'].index(/Ornament/i)
+    csv << [
+      ornament['displayProperties']['name'],
+      ornament['displayProperties']['description'],
+      "https://bungie.net#{ornament['displayProperties']['icon']}"
+    ]
+  end
+end
