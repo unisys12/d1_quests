9a1328013920d7262e164116c85e8a1a61656ae8
Phillip Jackson <unisys12@gmail.com>
2017-09-27 07:32:37 -0500

    use new data collections

--- app/d2/weapons.rb
+++ app/d2/weapons.rb
@@ -3,20 +3,20 @@ require 'bundler/setup'
 require 'mongo'
 require 'csv'
 require 'dotenv/load'
-require "open-uri"
+# require "open-uri"
 
 Mongo::Logger.level = Logger::FATAL
 
 client = Mongo::Client.new([ENV['DB_URL']], database: ENV['DB_USER'], user: ENV['DB_USER'], password: ENV['DB_PASSWORD'])
 
-item_defs = client['destiny2.manifest.en.DestinyInventoryItemDefinition']
-@category_defs = client['destiny2.manifest.en.DestinyItemCategoryDefinition']
+item_defs = client['destiny2.en.DestinyInventoryItemDefinition']
+@category_defs = client['destiny2.en.DestinyItemCategoryDefinition']
 
 @weapons = item_defs.find(itemCategoryHashes: 1)
 
 def resolve_category(hash)
-  category = @category_defs.find(_id: hash)
-  category.each do |cat|
+  categories = @category_defs.find(_id: hash)
+  categories.each do |cat|
     return cat['displayProperties']['name']
   end
 end
@@ -34,33 +34,33 @@ def list_weapons(hash)
         "https://bungie.net#{weapon['displayProperties']['icon']}",
         "https://bungie.net#{weapon['screenshot']}"
       ]
-      next if weapon['displayProperties']['name'].include?('/')
-      File.open("#{category}_imgs/#{weapon['displayProperties']['name']}.jpg", 'w+') do |fo|
-        fo.write open("https://bungie.net#{weapon['screenshot']}").read
-      end
+      # next if weapon['displayProperties']['name'].include?('/')
+      # File.open("#{category}_imgs/#{weapon['displayProperties']['name']}.jpg", 'w+') do |fo|
+      #   fo.write open("https://bungie.net#{weapon['screenshot']}").read
+      # end
     end
   end
 end
 
-puts '--- Power Weapons ---'
-list_weapons(4)
+# puts '--- Power Weapons ---'
+# list_weapons(4)
 
-puts '--- Energy Weapons ---'
-list_weapons(3)
+# puts '--- Energy Weapons ---'
+# list_weapons(3)
 
-puts '--- Kinetic Weapons ---'
-list_weapons(2)
+# puts '--- Kinetic Weapons ---'
+# list_weapons(2)
 
 # ALL WEAPONS
-# CSV.open('d2_weapons_simple.csv', 'wb') do |csv|
-#   csv << %w[name flavor_text weapon_type image_url screenshot_url]
-#   @weapons.each do |weapon|
-#     csv << [
-#       weapon['displayProperties']['name'],
-#       weapon['displayProperties']['description'],
-#       weapon['itemTypeDisplayName'],
-#       "https://bungie.net#{weapon['displayProperties']['icon']}",
-#       "https://bungie.net#{weapon['screenshot']}"
-#     ]
-#   end
-# end
+CSV.open('d2_weapons_simple.csv', 'wb') do |csv|
+  csv << %w[name flavor_text weapon_type image_url screenshot_url]
+  @weapons.each do |weapon|
+    csv << [
+      weapon['displayProperties']['name'],
+      weapon['displayProperties']['description'],
+      weapon['itemTypeDisplayName'],
+      "https://bungie.net#{weapon['displayProperties']['icon']}",
+      "https://bungie.net#{weapon['screenshot']}"
+    ]
+  end
+end
