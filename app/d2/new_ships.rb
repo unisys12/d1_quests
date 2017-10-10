40a659164a40e4e403a90bdc654cb176476db6d9
Phillip Jackson <unisys12@gmail.com>
2017-10-10 12:30:49 -0500

    first attempt to find new items

---
+++ app/d2/new_ships.rb
@@ -0,0 +1,28 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'csv'
+require 'dotenv/load'
+require 'open-uri'
+require_relative '../db/connect'
+
+client = DB.new(ENV['DB_URL'], ENV['DB_USER'], ENV['DB_USER'], ENV['DB_PASSWORD'])
+db = client.conn
+
+item_defs = db['destiny2.en.DestinyInventoryItemDefinition']
+
+@ships = item_defs.find(itemCategoryHashes: 42)
+
+CSV.foreach('d2_ships_simple.csv') do |row|
+  CSV.open('new_ships.csv', 'wb') do |csv|
+    csv << %w[name flavor_text image_url screenshot_url]
+    @ships.each do |ship|
+      next if row[0] == ship['displayProperties']['name']
+      csv << [
+        ship['displayProperties']['name'],
+        ship['displayProperties']['description'],
+        "https://bungie.net#{ship['displayProperties']['icon']}",
+        "https://bungie.net#{ship['screenshot']}"
+      ]
+    end
+  end
+end
