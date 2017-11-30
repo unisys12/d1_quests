4475a9bb3cd7e9b5425ae6d95a2dbd221bf15ace
Phillip Jackson <unisys12@gmail.com>
2017-11-30 17:14:32 -0600

    fetch all d1 items

---
+++ app/all_items.rb
@@ -0,0 +1,31 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'mongo'
+require 'csv'
+require 'dotenv/load'
+require_relative './db/connect'
+
+client = DB.new(ENV['DB_URL'], ENV['DB_USER'], ENV['DB_USER'], ENV['DB_PASSWORD'])
+db = client.conn
+
+item_defs = db['destiny.manifest.en.DestinyInventoryItemDefinition']
+@all_items = item_defs.find()
+
+CSV.open('d1_all_items.csv', 'wb') do |csv|
+  csv << %w[
+    name
+    description
+    tierTypeName
+    itemTypeName
+  ]
+
+  @all_items.each do |item|
+    puts '. '
+    csv << [
+      item['itemName'],
+      item['itemDescription'],
+      item['tierTypeName'],
+      item['itemTypeName'],
+    ]
+  end
+end
