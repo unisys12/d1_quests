477a7a07376443b883db464141b6d70772f62cb1
Phillip Jackson <unisys12@gmail.com>
2018-02-05 07:40:55 -0600

    remove commented code that was refactored

--- app/d2/ships.rb
+++ app/d2/ships.rb
@@ -27,39 +27,6 @@ def update_ships
   end
 end
 
-# def write_new(updates)
-#   puts "Writing file containing #{updates.count} new Ships from this update..."
-#   CSV.open("d2_new_Ships_#{Date.today}.csv", 'wb') do |csv|
-#     csv << %w[name flavor_text image_url screenshot_url]
-#     updates.each do |update|
-#       csv << update
-#     end
-#   end
-# end
-
-# def compare_ships
-#   arr_updates = []
-#   puts 'Comparing Ships...'
-#   old = File.open('d2_ships_simple_2018-01-16.csv')
-#   update = File.open("d2_ships_simple_#{Date.today}.csv")
-
-#   arr_a = []
-
-#   old_lines = old.readlines
-#   update_lines = update.readlines
-
-#   old_lines.each do |e|
-#     arr_a.push(e)
-#   end
-
-#   update_lines.each do |f|
-#     # puts f unless arr_a.include?(f)
-#     arr_updates.push(f.parse_csv) unless arr_a.include?(f)
-#   end
-#   write_new(arr_updates) if arr_updates.count > 0
-#   puts 'No new Ships found in this update...' if arr_updates.count.zero?
-# end
-
 # @ships.each do |ship|
 #   name = ship['displayProperties']['name']
 #   next if ship['displayProperties']['name'].include?('/')
