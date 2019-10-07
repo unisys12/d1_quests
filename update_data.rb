d6c4806c367c3081a39e6f6aff0a28c2dd1273eb
Phillip Jackson <unisys12@gmail.com>
2019-10-07 12:27:20 -0500

    remove compare actions for now

--- update_data.rb
+++ update_data.rb
@@ -8,11 +8,11 @@ require_relative './app/d2/ships.rb'
 require_relative './app/d2/sparrows.rb'
 require_relative './app/d2/treasure_maps.rb'
 require_relative './app/d2/weapons.rb'
-require './app/helpers/compare_items.rb'
+# require './app/helpers/compare_items.rb'
 # File to compare update against
-old_file_date = '2018-01-16'
+# old_file_date = '2018-01-16'
 
-update_item_sets
+# update_item_sets
 
 puts '--- Warlock Armor ---'
 update_armor(21)
@@ -23,37 +23,37 @@ update_armor(22)
 puts '--- Hunter Armor ---'
 update_armor(23)
 
-hunter = CompareItems.new('hunter', old_file_date)
-hunter.compare
+# hunter = CompareItems.new('hunter', old_file_date)
+# hunter.compare
 
-titan = CompareItems.new('titan', old_file_date)
-titan.compare
+# titan = CompareItems.new('titan', old_file_date)
+# titan.compare
 
-warlock = CompareItems.new('warlock', old_file_date)
-warlock.compare
+# warlock = CompareItems.new('warlock', old_file_date)
+# warlock.compare
 # compare_armor("Hunter")
 # compare_armor("Titan")
 # compare_armor("Warlock")
 
 update_emblems
-emblems = CompareItems.new('emblems', old_file_date)
-emblems.compare
+# emblems = CompareItems.new('emblems', old_file_date)
+# emblems.compare
 
 update_ghosts
-ghosts = CompareItems.new('ghosts', old_file_date)
-ghosts.compare
+# ghosts = CompareItems.new('ghosts', old_file_date)
+# ghosts.compare
 
 update_ornaments
-ornaments = CompareItems.new('ornaments', old_file_date)
-ornaments.compare
+# ornaments = CompareItems.new('ornaments', old_file_date)
+# ornaments.compare
 
 update_shaders
-shaders = CompareItems.new('shaders', old_file_date)
-shaders.compare
+# shaders = CompareItems.new('shaders', old_file_date)
+# shaders.compare
 
 update_ships
-ships = CompareItems.new('ships', old_file_date)
-ships.compare
+# ships = CompareItems.new('ships', old_file_date)
+# ships.compare
 
 update_sparrows
 # compare_sparrows
@@ -72,5 +72,5 @@ puts '--- Kinetic Weapons ---'
 update_weapon_groups(2)
 
 update_all_weapons
-weapons = CompareItems.new('weapons', old_file_date)
-weapons.compare
+# weapons = CompareItems.new('weapons', old_file_date)
+# weapons.compare
