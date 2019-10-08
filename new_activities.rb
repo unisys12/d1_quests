775f2cf457d76ff922684bb6c5bbb78403653180
Phillip Jackson <unisys12@gmail.com>
2019-10-08 12:52:22 -0500

    remove commented code

--- new_activities.rb
+++ new_activities.rb
@@ -21,21 +21,3 @@ destinations.each do |destination|
     dest_strikes = CompareActivities.new('./app/d2/pre_shadowkeep/' + destination + '_strikes.csv', './app/d2/shadowkeep/' + destination + '_strikes.csv')
     dest_strikes.compare
 end
-
-# earth_stories = CompareActivities.new('./app/d2/pre_shadowkeep/Earth_stories.csv', './app/d2/shadowkeep/Earth_stories.csv')
-# earth_stories.compare
-
-# earth_adventures = CompareActivities.new('./app/d2/pre_shadowkeep/Earth_adventures.csv', './app/d2/shadowkeep/Earth_adventures.csv')
-# earth_adventures.compare
-
-# earth_strikes = CompareActivities.new('./app/d2/pre_shadowkeep/Earth_strikes.csv', './app/d2/shadowkeep/Earth_strikes.csv')
-# earth_strikes.compare
-
-# io_stories = CompareActivities.new('./app/d2/pre_shadowkeep/io_stories.csv', './app/d2/shadowkeep/io_stories.csv')
-# io_stories.compare
-
-# io_adventures = CompareActivities.new('./app/d2/pre_shadowkeep/io_adventures.csv', './app/d2/shadowkeep/io_adventures.csv')
-# io_adventures.compare
-
-# io_strikes = CompareActivities.new('./app/d2/pre_shadowkeep/io_strikes.csv', './app/d2/shadowkeep/io_strikes.csv')
-# io_strikes.compare
